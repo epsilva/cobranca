2073977c3e970629c448a44a14fe035699da15d6
Esdras <eps.94@hotmail.com>
2016-12-09 12:33:33 -0200

    Criando metodo salvar salvando na base temporaria

---
+++ src/main/java/com/pinheiro/esdras/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.pinheiro.esdras.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pinheiro.esdras.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
