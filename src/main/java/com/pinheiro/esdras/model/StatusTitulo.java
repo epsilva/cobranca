b5e7798e5a05fc6850fd4dc96f60ca28b8bae168
epsilva <eps.94@hotmail.com>
2016-12-15 23:35:15 -0200

    Adicionando combo dinamica

--- src/main/java/com/pinheiro/esdras/model/StatusTitulo.java
+++ src/main/java/com/pinheiro/esdras/model/StatusTitulo.java
@@ -3,7 +3,8 @@ package com.pinheiro.esdras.model;
 public enum StatusTitulo {
 
 	PENDENTE("Pendente"),
-	RECEBIDO("Recebido");
+	RECEBIDO("Recebido"),
+	CANCELADO("Cancelado");
 	
 	private String descricao;
 	
